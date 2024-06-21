f17944fb8c3a722d37b80b4568851584baaffabe
lesterli <fulfilself@gmail.com>
2024-06-21 09:28:04 -0400

    feat: query voting power (#18)

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	"github.com/babylonchain/babylon-da-sdk/sdk/btc"
+	btcstakingtypes "github.com/babylonchain/babylon/x/btcstaking/types"
 	sdkquerytypes "github.com/cosmos/cosmos-sdk/types/query"
 )
 
@@ -113,14 +114,85 @@ func (babylonClient *babylonQueryClient) queryConsumerId() (string, error) {
 	return data.ConsumerId, nil
 }
 
-func queryMultiFpPowerAtHeight(fps []string, btcHeight uint64) (map[string]uint64, error) {
-	// TODO: implement
-	return map[string]uint64{
-		"pk1": 12345,
-		"pk2": 67890,
-		"pk3": 111213,
-		"pk4": 141516,
-	}, nil
+func (babylonClient *babylonQueryClient) queryMultiFpPowerAtHeight(fpPubkeyHexList []string, btcHeight uint64) (map[string]uint64, error) {
+	fpPowerMap := make(map[string]uint64)
+
+	for _, fpPubkeyHex := range fpPubkeyHexList {
+		fpPower, err := babylonClient.queryFpPower(fpPubkeyHex, btcHeight)
+		if err != nil {
+			return nil, err
+		}
+		fpPowerMap[fpPubkeyHex] = fpPower
+	}
+
+	return fpPowerMap, nil
+}
+
+// we implemented exact logic as in
+// https://github.com/babylonchain/babylon-private/blob/c5a8d317091e2965e20ea56fa10e98d34aaa3547/x/btcstaking/types/btc_delegation.go#L111-L119
+func (babylonClient *babylonQueryClient) isDelegationEligible(btcDel *btcstakingtypes.BTCDelegationResponse, btcHeight uint64) (bool, error) {
+	btccheckpointParams, err := babylonClient.bbnClient.QueryClient.BTCCheckpointParams()
+	if err != nil {
+		return false, err
+	}
+	btcstakingParams, err := babylonClient.bbnClient.QueryClient.BTCStakingParams()
+	if err != nil {
+		return false, err
+	}
+	wValue := btccheckpointParams.GetParams().CheckpointFinalizationTimeout
+	covQuorum := btcstakingParams.GetParams().CovenantQuorum
+	ud := btcDel.UndelegationResponse
+
+	if len(ud.GetDelegatorUnbondingSigHex()) > 0 {
+		return false, nil
+	}
+
+	if btcHeight < btcDel.StartHeight || btcHeight+wValue > btcDel.EndHeight {
+		return false, nil
+	}
+
+	if uint32(len(btcDel.CovenantSigs)) < covQuorum {
+		return false, nil
+	}
+	if len(ud.CovenantUnbondingSigList) < int(covQuorum) {
+		return false, nil
+	}
+	if len(ud.CovenantSlashingSigs) < int(covQuorum) {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (babylonClient *babylonQueryClient) queryFpPower(fpPubkeyHex string, btcHeight uint64) (uint64, error) {
+	totalPower := uint64(0)
+	pagination := &sdkquerytypes.PageRequest{}
+	// queries the BTCStaking module for all delegations of a finality provider
+	resp, err := babylonClient.bbnClient.QueryClient.FinalityProviderDelegations(fpPubkeyHex, pagination)
+	if err != nil {
+		return 0, err
+	}
+	for {
+		// btcDels contains all the queried BTC delegations
+		for _, btcDels := range resp.BtcDelegatorDelegations {
+			for _, btcDel := range btcDels.Dels {
+				// check the eligbility of delegation
+				isEligible, err := babylonClient.isDelegationEligible(btcDel, btcHeight)
+				if err != nil {
+					return 0, err
+				}
+				if isEligible {
+					totalPower += btcDel.TotalSat
+				}
+			}
+		}
+		if resp.Pagination == nil || resp.Pagination.NextKey == nil {
+			break
+		}
+		pagination.Key = resp.Pagination.NextKey
+	}
+
+	return totalPower, nil
 }
 
 func (babylonClient *babylonQueryClient) QueryIsBlockBabylonFinalized(queryParams QueryParams) (bool, error) {
@@ -143,7 +215,7 @@ func (babylonClient *babylonQueryClient) QueryIsBlockBabylonFinalized(queryParam
 	}
 
 	// get all FPs voting power at this BTC height
-	allFpPower, err := queryMultiFpPowerAtHeight(allFpPks, btcblockHeight)
+	allFpPower, err := babylonClient.queryMultiFpPowerAtHeight(allFpPks, btcblockHeight)
 	if err != nil {
 		return false, err
 	}
