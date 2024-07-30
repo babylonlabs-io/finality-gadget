00dd6dfb66ecd6fd59eb507fea21f4f0ee418fd8
parketh <yeungparkhay@gmail.com>
2024-07-30 14:49:34 +0800

    chore: fix formatting

--- verifier/config/config.go
+++ verifier/config/config.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Config struct {
-	L2RPCHost        		string 					`long:"l2-rpc-host" description:"rpc host address of the L2 node"`
-	BitcoinRPCHost      string 					`long:"bitcoin-rpc-host" description:"rpc host address of the bitcoin node"`
-	FGContractAddress 	string 					`long:"fg-contract-address" description:"BabylonChain op finality gadget contract address"`
-	BBNChainID 					string 					`long:"bbn-chain-id" description:"BabylonChain chain ID"`
-	BBNRPCAddress 			string 					`long:"bbn-rpc-address" description:"BabylonChain chain RPC address"`
-	DBFilePath 					string 					`long:"db-file-path" description:"path to the DB file"`
-	ServerPort				 	string 					`long:"server-port" description:"port to start the verifier server"`
-	PollInterval				time.Duration		`long:"retry-interval" description:"interval in seconds to recheck Babylon finality of block"`
+	L2RPCHost         string        `long:"l2-rpc-host" description:"rpc host address of the L2 node"`
+	BitcoinRPCHost    string        `long:"bitcoin-rpc-host" description:"rpc host address of the bitcoin node"`
+	FGContractAddress string        `long:"fg-contract-address" description:"BabylonChain op finality gadget contract address"`
+	BBNChainID        string        `long:"bbn-chain-id" description:"BabylonChain chain ID"`
+	BBNRPCAddress     string        `long:"bbn-rpc-address" description:"BabylonChain chain RPC address"`
+	DBFilePath        string        `long:"db-file-path" description:"path to the DB file"`
+	ServerPort        string        `long:"server-port" description:"port to start the verifier server"`
+	PollInterval      time.Duration `long:"retry-interval" description:"interval in seconds to recheck Babylon finality of block"`
 }
 
 func Load(configPath string) (*Config, error) {
@@ -22,12 +22,12 @@ func Load(configPath string) (*Config, error) {
 	viper.SetConfigType("toml")
 
 	if err := viper.ReadInConfig(); err != nil {
-			return nil, err
+		return nil, err
 	}
 
 	var config Config
 	if err := viper.Unmarshal(&config); err != nil {
-			return nil, err
+		return nil, err
 	}
 
 	return &config, nil
