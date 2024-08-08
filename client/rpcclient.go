9d7b41d3389a2bf6e411edb5c4f0ad4c9d2f82ac
parketh <yeungparkhay@gmail.com>
2024-08-08 17:22:27 +0100

    feat: add grpc client routes

--- client/rpcclient.go
+++ client/rpcclient.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"fmt"
+	"math"
 
 	"github.com/babylonlabs-io/finality-gadget/proto"
 	"github.com/babylonlabs-io/finality-gadget/types"
@@ -29,6 +30,55 @@ func NewFinalityGadgetGrpcClient(
 	}
 
 	return gClient, nil
+}
+
+func (c *FinalityGadgetGrpcClient) QueryIsBlockBabylonFinalized(block *types.Block) (bool, error) {
+	req := &proto.QueryIsBlockBabylonFinalizedRequest{
+		Block: &proto.BlockInfo{
+			BlockHash:      block.BlockHash,
+			BlockHeight:    block.BlockHeight,
+			BlockTimestamp: block.BlockTimestamp,
+		},
+	}
+
+	res, err := c.client.QueryIsBlockBabylonFinalized(context.Background(), req)
+	if err != nil {
+		return false, err
+	}
+
+	return res.IsFinalized, nil
+}
+
+func (c *FinalityGadgetGrpcClient) QueryBlockRangeBabylonFinalized(blocks []*types.Block) (*uint64, error) {
+	b := make([]*proto.BlockInfo, 0, len(blocks))
+	for _, block := range blocks {
+		b = append(b, &proto.BlockInfo{
+			BlockHash:      block.BlockHash,
+			BlockHeight:    block.BlockHeight,
+			BlockTimestamp: block.BlockTimestamp,
+		})
+	}
+	req := &proto.QueryBlockRangeBabylonFinalizedRequest{
+		Blocks: b,
+	}
+
+	res, err := c.client.QueryBlockRangeBabylonFinalized(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res.LastFinalizedBlockHeight, nil
+}
+
+func (c *FinalityGadgetGrpcClient) QueryBtcStakingActivatedTimestamp() (uint64, error) {
+	req := &proto.QueryBtcStakingActivatedTimestampRequest{}
+
+	res, err := c.client.QueryBtcStakingActivatedTimestamp(context.Background(), req)
+	if err != nil {
+		return math.MaxUint64, err
+	}
+
+	return res.ActivatedTimestamp, nil
 }
 
 func (c *FinalityGadgetGrpcClient) QueryIsBlockFinalizedByHeight(height uint64) (bool, error) {
@@ -66,9 +116,9 @@ func (c *FinalityGadgetGrpcClient) QueryLatestFinalizedBlock() (*types.Block, er
 	}
 
 	return &types.Block{
-		BlockHash:      res.BlockHash,
-		BlockHeight:    res.BlockHeight,
-		BlockTimestamp: res.BlockTimestamp,
+		BlockHash:      res.Block.BlockHash,
+		BlockHeight:    res.Block.BlockHeight,
+		BlockTimestamp: res.Block.BlockTimestamp,
 	}, nil
 }
 
